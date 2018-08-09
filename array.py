d0925deb5bf699066b21a681c93ff4ce3038803d
xushuguo <xushuguo@163.com>
2018-08-09 21:52:52 +0800

    提交仓库

---
+++ array.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+import numpy as np
+a = np.arange(10)
+print(a)
+b = np.arange(1,10)
+print(b)
+c = np.arange(1,10,2)
+print(c)
+# d = np.arange([])
+# print(d)
+e = np.array([10,20,30,40,50])
+print(e)
+print(e.dtype())
+# f = np.array([
+# 	[1,2,3],
+# 	[4,5,6]])	
+# print(f)
+# print(type(f))
+# print(f[0][0])
+# print(f.dtype)
