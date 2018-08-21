45661642e581d5708a47e2042d1a6eb780e1a29c
xushuguo <xushuguo@163.com>
2018-08-21 09:30:04 +0800

    add file

---
+++ test_2.py
@@ -0,0 +1,18 @@
+# 字符串的次数与元素，可以以":"对的存在
+
+a = "adsbdjsaasdd"
+# 存入列表
+l = list(a)
+l1 = []
+# 备用字典
+d ={}
+# 去重
+l2 = list(set(l))
+
+for i in l2:
+# 存入字典
+	for j in zip()
+	d[i]=l.count(i)
+# 排序
+dd = sorted(d.items(),key = lambda x:x[1],reverse = True)
+print(dd)
